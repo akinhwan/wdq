c1ce82fb69d8ee10b09430edb82eebde99ab8cdc
Andrew Kim <akinhwan@gmail.com>
2021-01-18 13:42:53 -0500

    add convertkit, title

--- src/partials/Cta.vue
+++ src/partials/Cta.vue
@@ -33,14 +33,45 @@
           </div>
 
           <!-- CTA form -->
-          <form class="w-full lg:w-1/2">
+          <form action="https://app.convertkit.com/forms/1967626/subscriptions"
+            class="w-full lg:w-1/2 seva-form formkit-form" method="post" data-sv-form="1967626" data-uid="f0509ae312"
+            data-format="inline" data-version="5"
+            data-options="{&quot;settings&quot;:{&quot;after_subscribe&quot;:{&quot;action&quot;:&quot;message&quot;,&quot;success_message&quot;:&quot;Success! Now check your email to confirm your subscription.&quot;,&quot;redirect_url&quot;:&quot;&quot;},&quot;analytics&quot;:{&quot;google&quot;:null,&quot;facebook&quot;:null,&quot;segment&quot;:null,&quot;pinterest&quot;:null,&quot;sparkloop&quot;:null,&quot;googletagmanager&quot;:null},&quot;modal&quot;:{&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;powered_by&quot;:{&quot;show&quot;:true,&quot;url&quot;:&quot;https://convertkit.com?utm_source=dynamic&amp;utm_medium=referral&amp;utm_campaign=poweredby&amp;utm_content=form&quot;},&quot;recaptcha&quot;:{&quot;enabled&quot;:false},&quot;return_visitor&quot;:{&quot;action&quot;:&quot;show&quot;,&quot;custom_content&quot;:&quot;&quot;},&quot;slide_in&quot;:{&quot;display_in&quot;:&quot;bottom_right&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;sticky_bar&quot;:{&quot;display_in&quot;:&quot;top&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15}},&quot;version&quot;:&quot;5&quot;}"
+            min-width="400 500 600 700 800">
+            <div data-style="clean flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:max-w-none">
+              <ul class="formkit-alert formkit-alert-error" data-element="errors" data-group="alert"></ul>
+              <div data-element="fields" data-stacked="false" class="seva-fields formkit-fields">
+                <div class="formkit-field">
+                  <input class="formkit-input form-input w-full mb-2 sm:mb-0 sm:mr-2" name="email_address"
+                    style="color: rgb(0, 0, 0); border-color: rgb(227, 227, 227); border-radius: 4px; font-weight: 400;"
+                    aria-label="Email Address" placeholder="Email Address" required="" type="email">
+                    </div>
+                    <button
+                  data-element="submit" class="formkit-submit formkit-submit btn text-white bg-teal-500 hover:bg-teal-400 flex-shrink-0"
+                  style="color: rgb(255, 255, 255); background-color: rgb(22, 119, 190); border-radius: 4px; font-weight: 400;">
+                  <div class="formkit-spinner">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                  </div><span class="bg-teal-500 hover:bg-teal-400 flex-shrink-0">Subscribe</span>
+                </button>
+              </div>
+              <div class="formkit-powered-by-convertkit-container"><a
+                  href="https://convertkit.com?utm_source=dynamic&amp;utm_medium=referral&amp;utm_campaign=poweredby&amp;utm_content=form"
+                  data-element="powered-by" class="formkit-powered-by-convertkit" target="_blank"
+                  rel="noopener noreferrer">Built with ConvertKit</a></div>
+            </div>
+          </form>
+
+
+          <!-- <form class="w-full lg:w-1/2">
             <div class="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:max-w-none">
               <input type="tel" class="form-input w-full mb-2 sm:mb-0 sm:mr-2" placeholder="Phone number" aria-label="Phone number" />
               <a class="btn text-white bg-teal-500 hover:bg-teal-400 flex-shrink-0" href="#0">Request code</a>
             </div>
-            <!-- Success message -->
-            <!-- <p class="text-center lg:text-left lg:absolute mt-2 opacity-75 text-sm">Thanks for subscribing!</p> -->
-          </form>
+            Success message
+            <p class="text-center lg:text-left lg:absolute mt-2 opacity-75 text-sm">Thanks for subscribing!</p>
+          </form> -->
 
         </div>
 
--- src/partials/HeroHome.vue
+++ src/partials/HeroHome.vue
@@ -9,16 +9,40 @@
           <!-- Content -->
           <div class="md:col-span-7 lg:col-span-7 mb-8 md:mb-0 text-center md:text-left">
             <h1 class="h1 lg:text-6xl mb-4 font-red-hat-display font-extrabold" data-aos="fade-down">Speedruns of workflows</h1>
-            <p class="text-xl text-gray-600 dark:text-gray-400" data-aos="fade-down" data-aos-delay="150">Can you beat the fastest record time for completing your workflow?</p>
+            <p class="text-xl text-gray-600 dark:text-gray-400" data-aos="fade-down" data-aos-delay="150">Can you beat the fastest recorded time for completing <i>your</i> workflow?</p>
             <!-- CTA form -->
-            <form class="mt-8" data-aos="fade-down" data-aos-delay="300">
-              <div class="flex flex-col sm:flex-row justify-center max-w-sm mx-auto sm:max-w-md md:mx-0">
-                <input type="tel" class="form-input w-full mb-2 sm:mb-0 sm:mr-2" placeholder="Phone number" aria-label="Phone number" />
-                <a class="btn text-white bg-teal-500 hover:bg-teal-400 flex-shrink-0" href="#0">Request code</a>
+            
+
+            <form data-aos="fade-down" data-aos-delay="300" action="https://app.convertkit.com/forms/1967626/subscriptions"
+            class="mt-8 seva-form formkit-form" method="post" data-sv-form="1967626" data-uid="f0509ae312"
+            data-format="inline" data-version="5"
+            data-options="{&quot;settings&quot;:{&quot;after_subscribe&quot;:{&quot;action&quot;:&quot;message&quot;,&quot;success_message&quot;:&quot;Success! Now check your email to confirm your subscription.&quot;,&quot;redirect_url&quot;:&quot;&quot;},&quot;analytics&quot;:{&quot;google&quot;:null,&quot;facebook&quot;:null,&quot;segment&quot;:null,&quot;pinterest&quot;:null,&quot;sparkloop&quot;:null,&quot;googletagmanager&quot;:null},&quot;modal&quot;:{&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;powered_by&quot;:{&quot;show&quot;:true,&quot;url&quot;:&quot;https://convertkit.com?utm_source=dynamic&amp;utm_medium=referral&amp;utm_campaign=poweredby&amp;utm_content=form&quot;},&quot;recaptcha&quot;:{&quot;enabled&quot;:false},&quot;return_visitor&quot;:{&quot;action&quot;:&quot;show&quot;,&quot;custom_content&quot;:&quot;&quot;},&quot;slide_in&quot;:{&quot;display_in&quot;:&quot;bottom_right&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;sticky_bar&quot;:{&quot;display_in&quot;:&quot;top&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15}},&quot;version&quot;:&quot;5&quot;}"
+            min-width="400 500 600 700 800">
+            <div data-style="clean flex flex-col sm:flex-row justify-center max-w-sm mx-auto sm:max-w-md md:mx-0">
+              <ul class="formkit-alert formkit-alert-error" data-element="errors" data-group="alert"></ul>
+              <div data-element="fields" data-stacked="false" class="seva-fields formkit-fields">
+                <div class="formkit-field">
+                  <input class="formkit-input form-input w-full mb-2 sm:mb-0 sm:mr-2" name="email_address"
+                    style="color: rgb(0, 0, 0); border-color: rgb(227, 227, 227); border-radius: 4px; font-weight: 400;"
+                    aria-label="Email Address" placeholder="Email Address" required="" type="email">
+                    </div>
+                    <button
+                  data-element="submit" class="formkit-submit formkit-submit btn text-white bg-teal-500 hover:bg-teal-400 flex-shrink-0"
+                  style="color: rgb(255, 255, 255); background-color: rgb(22, 119, 190); border-radius: 4px; font-weight: 400;">
+                  <div class="formkit-spinner">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                  </div><span class="bg-teal-500 hover:bg-teal-400 flex-shrink-0">Subscribe</span>
+                </button>
               </div>
-              <!-- Success message -->
-              <!-- <p class="text-center md:text-left mt-2 opacity-75 text-sm">Thanks for subscribing!</p> -->
-            </form>
+              <!-- <div class="formkit-powered-by-convertkit-container"><a
+                  href="https://convertkit.com?utm_source=dynamic&amp;utm_medium=referral&amp;utm_campaign=poweredby&amp;utm_content=form"
+                  data-element="powered-by" class="formkit-powered-by-convertkit" target="_blank"
+                  rel="noopener noreferrer">Built with ConvertKit</a></div> -->
+            </div>
+          </form>
+
             <ul class="max-w-sm sm:max-w-md mx-auto md:max-w-none text-gray-600 dark:text-gray-400 mt-8 -mb-2" data-aos="fade-down" data-aos-delay="450">
               <li class="flex items-center mb-2">
                 <svg class="w-3 h-3 fill-current text-teal-400 mr-2 flex-shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
